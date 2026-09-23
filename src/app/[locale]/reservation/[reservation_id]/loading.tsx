f70424dcccc29c4c9e4656cb7b18926bb478e775
elgafy <abdalaziz.elgafy@gmail.com>
2026-09-23 13:35:28 +0300

    Added loader to reservation confirmation page

--- src/app/[locale]/reservation/[reservation_id]/loading.tsx
+++ src/app/[locale]/reservation/[reservation_id]/loading.tsx
@@ -1,9 +1,8 @@
 export default function Loading() {
   return (
-    <main className="h-screen w-screen flex items-center justify-center bg-black/50 ">
-      <section className="relative">
-        <div className="w-[203px] mobile:w-[150px] mobile:border-[10px] aspect-square rounded-full border-[30px]  z-50 border-white/30 border-r-mainColor animate-spin"></div>
-      </section>
-    </main>
+    <div className="">
+      <div className="loader"></div>
+      <div className="loader-container flex"></div>
+    </div>
   );
 }
